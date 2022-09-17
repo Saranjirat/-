9cc53d37a97c4f801c4edd0b131aba2fc73a0c5f
Saranjirat <110214803+Saranjirat@users.noreply.github.com>
2022-09-18 00:44:01 +0700

    Add files via upload

---
+++ averageheight.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    float q,w,e,r,t,y,u,i,o,p,x;
+    printf("student 1 = ");
+    scanf("%f",&q);
+    printf("student 2 = ");
+    scanf("%f",&w);
+    printf("student 3 = ");
+    scanf("%f",&e);
+    printf("student 4 = ");
+    scanf("%f",&r);
+    printf("student 5 = ");
+    scanf("%f",&t);
+    printf("student 6 = ");
+    scanf("%f",&y);
+    printf("student 7 = ");
+    scanf("%f",&u);
+    printf("student 8 = ");
+    scanf("%f",&i);
+    printf("student 9 = ");
+    scanf("%f",&o);
+    printf("student 10 = ");
+    scanf("%f",&p);
+    x=(q+w+e+r+t+y+u+i+o+p)/10;
+    printf("Average Height is %0.2f",x);
+    return 0;
+}
