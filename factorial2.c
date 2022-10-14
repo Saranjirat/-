29bc7afdd8767440321f6aa1422516e94e523b68
Saranjirat <110214803+Saranjirat@users.noreply.github.com>
2022-10-14 20:29:43 +0700

    Add files via upload

---
+++ factorial2.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int Factorial(int f){
+	if(f == 0 || f == 1)
+    {
+		return 1;
+	}
+	else
+    {
+		return f * Factorial(f-1);
+	}
+}
+
+int main(){
+	int f;
+	printf("Input number : ");
+	scanf("%d",&f);
+	if(f < 0)
+    {
+		printf("Error!");
+	}
+	else
+    {
+		printf("%d",Factorial(f));
+	}
+}
